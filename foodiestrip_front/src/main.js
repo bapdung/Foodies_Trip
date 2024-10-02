0e51cb757ba92bbc264b8c8a133807c6860bcb59
dbqudwn011 <dbqudwn011@naver.com>
2024-10-02 16:22:03 +0900

    hotfix: kakaoMap 수정

--- foodiestrip_front/src/main.js
+++ foodiestrip_front/src/main.js
@@ -1,18 +1,18 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import { useKakao } from "vue3-kakao-maps/@utils";
+import App from './App.vue';
+import { useKakao } from 'vue3-kakao-maps/@utils';
 
-import router from "./router";
+import router from './router';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-useKakao("58009bacc033e9770620c790caacb9c7");
+useKakao('58009bacc033e9770620c790caacb9c7', ['clusterer', 'services', 'drawing']);
 
-app.mount("#app");
+app.mount('#app');
